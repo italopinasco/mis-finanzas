fd8079f73e1f8e8a37c3f5cd77c095e3599651bb
italopinasco <italopinasco@gmail.com>
2026-08-21 14:51:45 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const C='finanzas-v1';const A=['./','./index.html','./styles.css','./app.js','./manifest.json','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
